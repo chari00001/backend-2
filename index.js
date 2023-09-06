2e4669d6f9da67c13184ad0a02b725e3d4b1e381
chari00001 <cagrilacin303@gmail.com>
2023-09-07 00:14:10 +0300

    removed next

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const next = require("next");
+// const next = require("next");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
@@ -14,34 +14,22 @@ const uploadRoutes = require("./src/routes/upload");
 const connectDB = require("./src/config/database");
 connectDB();
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const server = express();
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static("public"));
+server.use(cors());
 
-    server.use(bodyParser.json());
-    server.use(bodyParser.urlencoded({ extended: true }));
-    server.use(express.static("public"));
-    server.use(cors());
+server.use("/api/products", productRoutes);
+server.use("/api/users", userRoutes);
+server.use("/api/upload", uploadRoutes);
 
-    server.use("/api/products", productRoutes);
-    server.use("/api/users", userRoutes);
-    server.use("/api/upload", uploadRoutes);
+// server.get("*", (req, res) => {
+//   return handle(req, res);
+// });
 
-    server.get("*", (req, res) => {
-      return handle(req, res);
-    });
-
-    server.listen(process.env.PORT || 3001, (err) => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:3001");
-    });
-  })
-  .catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+server.listen(process.env.PORT || 3001, (err) => {
+  if (err) throw err;
+  console.log("> Ready on http://localhost:3001");
+});
